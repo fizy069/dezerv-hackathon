890fea226a00351487cc33d4ff49a0a256b4c0df
mahir <139711115+fizy069@users.noreply.github.com>
2025-03-23 05:24:11 +0530

    Delete backend directory

--- backend/models/index.js
+++
@@ -1,8 +0,0 @@
-const User = require("./user");
-const Expense = require("./expense");
-const Income = require("./income");
-const Budget = require("./budget");
-const Report = require("./report");
-const Trip = require("./trip");
-
-module.exports = { User, Expense, Income, Budget, Report, Trip };
